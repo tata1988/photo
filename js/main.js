088ae6b83efad46b85364d11aeb7ca7b7f0b5b0c
Tatyana <nikolina.tatyana@yandex.ru>
2023-01-02 02:29:07 +0500

    разметка секции about

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Initialize Swiper
+
+let swiper = new Swiper('.swiper', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
