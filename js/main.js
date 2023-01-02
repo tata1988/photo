f078eeedf05bb466ee491922f204ac0767323f1f
Tatyana <nikolina.tatyana@yandex.ru>
2023-01-03 02:14:28 +0500

    адаптив menu, header, about

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+$(document).ready(function () {
+    /* Плавная прокрутка меню */
+    $('.header a[href^="#"]').click(function () {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+        $('.header a[href^="#"]').parent().removeClass('active');
+        $(this).parent().addClass('active');
+        $('.menu__mobile .menu').toggle(500);
+        $('.menu__burger').toggleClass('close');
+        return false;
+    });
+    /* Мобильное меню */
+    $('.menu__burger').click(function () {
+        $('.menu__mobile .menu').toggle(500);
+        $(this).toggleClass('close');
+    });
+
+    
+});
+
+
 // Initialize Swiper
 
 let swiper = new Swiper('.swiper', {
